49fc029add34fa073c03e2b5b0bd05423f80e898
Shra <shrayswarup@gmail.com>
2023-10-25 18:15:55 -0700

    Homepage updates

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -12,20 +12,48 @@ export const Nav = () => {
 
   return (
     <nav className={styles.nav}>
-      <Link
-        className={`${styles.link} ${pathname === '/' ? styles.active : ''}`}
-        href="/"
-      >
-        Home
-      </Link>
-      <Link
-        className={`${styles.link} ${
-          pathname === '/verify' ? styles.active : ''
-        }`}
-        href="/verify"
-      >
-        Verify
-      </Link>
+      <img src="/logo_text.webp" alt="Logo" className={styles.logo} />
+
+      <div className={styles.navLinks}>
+        <Link
+          className={`${styles.link} ${pathname === '/' ? styles.active : ''}`}
+          href="/"
+        >
+          HOME
+        </Link>
+        <Link
+          className={`${styles.link} ${
+            pathname === '/comparisons' ? styles.active : ''
+          }`}
+          href="/pages/comparisons"
+        >
+          COMPARISONS
+        </Link>
+        <Link
+          className={`${styles.link} ${
+            pathname === '/peak-times' ? styles.active : ''
+          }`}
+          href="/pages/peak-times"
+        >
+          PEAK TIMES
+        </Link>
+        <Link
+          className={`${styles.link} ${
+            pathname === '/blog' ? styles.active : ''
+          }`}
+          href="/pages/blog"
+        >
+          BLOG
+        </Link>
+        <Link
+          className={`${styles.link} ${
+            pathname === '/about' ? styles.active : ''
+          }`}
+          href="/pages/about"
+        >
+          ABOUT
+        </Link>
+      </div>
     </nav>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,18 +10,23 @@ export default function RootLayout(props: React.PropsWithChildren) {
   return (
     <Providers>
       <html lang="en">
+        <title>Lobster Lines</title>
         <body>
           <section className={styles.container}>
             <Nav />
 
-            <header className={styles.header}>
+            {/* <header className={styles.header}>
               <img src="/logo.svg" className={styles.logo} alt="logo" />
-            </header>
+            </header> */}
 
             <main className={styles.main}>{props.children}</main>
 
             <footer className={styles.footer}>
-              <span>Learn </span>
+              <span>Copyright © 2023 <span className={styles.highlighted}>Lobster Lines</span>. All Rights Reserved</span>
+              <div className={styles.disclaimer}>
+                  Disclaimer: This site is not affiliated with or endorsed by Red Lobster.<br />All trademarks belong to their respective owners. Wait times are for<br />reference only; please verify with individual locations.
+              </div>
+              {/* <span>Learn </span>
               <a
                 className={styles.link}
                 href="https://reactjs.org/"
@@ -56,7 +61,7 @@ export default function RootLayout(props: React.PropsWithChildren) {
                 rel="noopener noreferrer"
               >
                 React Redux
-              </a>
+              </a> */}
             </footer>
           </section>
         </body>
