42c2e6d6397216b67c81141796b5e78752d07a42
Shra <shrayswarup@gmail.com>
2024-02-10 20:18:02 -0700

    Change Peak Times Page to Trends

---
+++ app/trends/page.tsx
@@ -0,0 +1,10 @@
+export default function TrendsPage() {
+  return (
+    <>
+      <h1>[WIP] Trends</h1>
+      <p>
+        Tracking historical changes in restaurant count and wait times.
+      </p>
+    </>
+  )
+}
